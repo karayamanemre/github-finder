dec8a2675a1d5eed578aa28b2e79e92613a0c5e2
Emre Karayaman <karayaman.emre@outlook.com>
2023-06-21 21:41:36 +0300

    navbar and footer

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer className='footer p-8 bg-gray-500 text-primary-content footer-center'>
+			<p>Github Finder {new Date().getFullYear()} &copy; </p>
+		</footer>
+	);
+};
+
+export default Footer;
